655fb94ca15ba624d7a17c2afa089c346ad48fe9
Ju <c20chin@gmail.com>
2023-10-16 16:12:10 +0100

    model binding

--- Blog.NETMVC/Controllers/AdminTagsController.cs
+++ Blog.NETMVC/Controllers/AdminTagsController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blog.NETMVC.Data;
+using Blog.NETMVC.Models.Domain;
 using Blog.NETMVC.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,19 +11,36 @@ namespace Blog.NETMVC.Controllers
 {
     public class AdminTagsController : Controller
     {
+        private readonly BlogDbContext blogDbContext;
+
+        public AdminTagsController(BlogDbContext blogDbContext)
+        {
+            this.blogDbContext = blogDbContext;
+        }
+
         [HttpGet]
         public IActionResult Add()  // http://localhost:xxxx/AdminTags/Add: To view the Add page
         {
             return View();
         }
 
-
+        
         [HttpPost]
         [ActionName("Add")]
         public IActionResult SubmitTag(AddTagRequest addTagRequest)
         {
-            var name = addTagRequest.Name;
-            var displayName = addTagRequest.DisplayName;
+            //Mapping AddTagRequest to Tag domain model
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName
+            };
+
+
+
+            blogDbContext.Tags.Add(tag);
+            blogDbContext.SaveChanges();
+            
 
             return View("Add");
         }
